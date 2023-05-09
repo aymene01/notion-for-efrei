40c0d90aae96b978c0fddc2bdbaa121123431845
aymene01 <aymene01@hotmail.fr>
2023-05-09 11:05:05 +0200

    login and register pages

--- apps/web/src/pages/auth/login.tsx
+++ apps/web/src/pages/auth/login.tsx
@@ -1,7 +1,53 @@
+import { Input, Button, Heading, InputGroup, InputRightElement } from '@chakra-ui/react'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import createSession from '@/lib/queries/createSession'
+import useSwrMutation from '@/lib/hooks/useSwrMutation'
+import { UserAuthenticated } from '@efrei/graphql'
+import { toast } from 'react-hot-toast'
+
+type LoginRequest = {
+  email: string
+  password: string
+}
+
 const Login = () => {
+  const [show, setShow] = React.useState<boolean>(false)
+  const { register, handleSubmit } = useForm<LoginRequest>()
+  const { mutate, isSuccess, isError, error } = useSwrMutation<UserAuthenticated, 'createSession'>(createSession)
+
+  const handleCreateAccount = (data: LoginRequest) => {
+    mutate(data)
+  }
+
+  const handleToggle = () => setShow(!show)
+
+  React.useEffect(() => {
+    if (isSuccess) {
+      toast.success('Valid credentials')
+    }
+    if (error) {
+      toast.error(error.message)
+    }
+  }, [isSuccess, error])
+
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <h1>Login</h1>
+    <div className="w-screen h-screen flex justify-center items-center flex-col space-y-4">
+      <Heading>Sign In</Heading>
+      <div className="flex items-center justify-center flex-col w-1/4 space-y-4">
+        <div className="space-y-2">
+          <Input placeholder="Email" {...register('email')} />
+          <InputGroup>
+            <Input placeholder="Password" {...register('password')} type={show ? 'text' : 'password'} />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={handleToggle}>
+                {show ? 'Hide' : 'Show'}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </div>
+        <Button onClick={handleSubmit(handleCreateAccount)}>Sign In</Button>
+      </div>
     </div>
   )
 }
--- apps/web/src/pages/auth/register.tsx
+++ apps/web/src/pages/auth/register.tsx
@@ -1,7 +1,50 @@
+import React from 'react'
+import { Heading, Button, Input } from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
+import { toast } from 'react-hot-toast'
+import { UserAuthenticated } from '@efrei/graphql'
+import createAccount from '@/lib/queries/createAccount'
+import useSwrMutation from '@/lib/hooks/useSwrMutation'
+
+type RegisterRequest = {
+  name: string
+  email: string
+  password: string
+}
+
 const Register = () => {
+  const { register, handleSubmit } = useForm<RegisterRequest>()
+  const { mutate, isSuccess, error } = useSwrMutation<UserAuthenticated, 'createAccount'>(createAccount)
+
+  const handleCreateAccount = (data: RegisterRequest) => {
+    mutate(data)
+  }
+
+  React.useEffect(() => {
+    if (isSuccess) {
+      toast.success('Account created')
+    }
+  }, [isSuccess])
+
+  React.useEffect(() => {
+    if (error) {
+      toast.error(error.message)
+    }
+  }, [error])
+
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <h1>Register</h1>
+      <div className="w-screen h-screen flex justify-center items-center flex-col space-y-4">
+        <Heading>Create your account</Heading>
+        <div className="flex items-center justify-center flex-col w-1/4 space-y-4">
+          <div className="space-y-2">
+            <Input placeholder="name" {...register('name')} />
+            <Input placeholder="email" {...register('email')} />
+            <Input placeholder="password" {...register('password')} type={'password'} />
+          </div>
+          <Button onClick={handleSubmit(handleCreateAccount)}>Create Account</Button>
+        </div>
+      </div>
     </div>
   )
 }
