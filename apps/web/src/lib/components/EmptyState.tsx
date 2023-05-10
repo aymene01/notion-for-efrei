cf510304c7a44c8922a6b9aab630b6f5c4a9c04d
aymene01 <aymene01@hotmail.fr>
2023-05-10 17:37:33 +0200

    remove comments

--- apps/web/src/lib/components/EmptyState.tsx
+++ apps/web/src/lib/components/EmptyState.tsx
@@ -1,4 +1,3 @@
-import { PlusIcon } from '@heroicons/react/20/solid'
 import { PostCreateButton } from './CreateNewPost'
 
 export default function EmptyState() {
@@ -22,14 +21,6 @@ export default function EmptyState() {
       <h3 className="mt-2 text-sm font-semibold text-gray-900">No Post</h3>
       <p className="mt-1 text-sm text-gray-500">Get started by creating a new post</p>
       <div className="mt-6">
-        {/* <button
-          type="button"
-          className="inline-flex items-center rounded-md bg-gray-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
-          New Post
-
-        </button> */}
         <PostCreateButton />
       </div>
     </div>
