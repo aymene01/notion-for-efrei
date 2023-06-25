97f2d9cccc9994bcb426e13eb8e6f306f33b2030
aymene01 <aymene01@hotmail.fr>
2023-06-25 09:12:40 +0200

    refacto DropDown

--- apps/web/src/lib/components/DropDown.tsx
+++ apps/web/src/lib/components/DropDown.tsx
@@ -4,6 +4,16 @@ import { Menu } from '@headlessui/react'
 
 export const DropDown = () => {
   const { logout, state } = useUser()
+
+  const menuItems = [
+    { label: state.email },
+    { label: 'Settings', href: '#' },
+    { label: 'Billing', href: '#' },
+    { label: 'Dashboard', href: '#' },
+    { label: 'Settings', href: '#' },
+    { label: 'Sign out', onClick: logout },
+  ]
+
   return (
     //@ts-ignore
     <Menu as="div" className="relative">
@@ -14,65 +24,29 @@ export const DropDown = () => {
       </div>
       <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
         <div className="">
-          <Menu.Item>
-            {({ active }) => (
-              <h1 className={`${active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'} block px-4 py-2 text-sm`}>
-                {state.email}
-              </h1>
-            )}
-          </Menu.Item>
-          <div className="w-full h-[1px] bg-gray-200" />
-          <Menu.Item>
-            {({ active }) => (
-              <a
-                href="#"
-                className={`${active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'} block px-4 py-2 text-sm`}
-              >
-                Settings
-              </a>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <a
-                href="#"
-                className={`${active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'} block px-4 py-2 text-sm`}
-              >
-                Billing
-              </a>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <a
-                href="#"
-                className={`${active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'} block px-4 py-2 text-sm`}
-              >
-                Dashboard
-              </a>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <a
-                href="#"
-                className={`${active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'} block px-4 py-2 text-sm`}
-              >
-                Settings
-              </a>
-            )}
-          </Menu.Item>
-          <div className="w-full h-[1px] bg-gray-200" />
-          <Menu.Item>
-            {({ active }) => (
-              <button
-                onClick={() => logout()}
-                className={`${active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'} block px-4 py-2 text-sm w-full`}
-              >
-                Sign out
-              </button>
-            )}
-          </Menu.Item>
+          {menuItems.map((item, index) => (
+            <Menu.Item key={index}>
+              {({ active }) => {
+                const commonClasses = `${
+                  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
+                } block px-4 py-2 text-sm`
+
+                if (item.onClick) {
+                  return (
+                    <button onClick={item.onClick} className={`${commonClasses} w-full`}>
+                      {item.label}
+                    </button>
+                  )
+                }
+
+                return (
+                  <a href={item.href} className={commonClasses}>
+                    {item.label}
+                  </a>
+                )
+              }}
+            </Menu.Item>
+          ))}
         </div>
       </Menu.Items>
     </Menu>
